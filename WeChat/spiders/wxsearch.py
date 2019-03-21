f3025d7eca37862a778e3c38533eec8e15d55943
ttttta <691989508@qq.com>
2019-03-22 00:06:34 +0800

    Signed-off-by: ttttta <691989508@qq.com>

--- WeChat/spiders/wxsearch.py
+++ WeChat/spiders/wxsearch.py
@@ -1,13 +1,24 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import sys
+from scrapy import Request
+from WeChat.items import WechatItem
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
 class WxsearchSpider(scrapy.Spider):
     name = 'wxsearch'
     #allowed_domains = ['www.baidu.com']#显示爬虫下载范围
-    start_urls = ['https://tieba.baidu.com/f?kw=剑网三交易&ie=utf-8&pn=0']#开始url，在下一次会自动变为下一个url
+
+    def start_requests(self):
+        start_urls = ['https://www.baidu.com/s?ie=utf-8&wd=a&pn={}'.format(page) for page in range(0,40,10)]#开始url，在下一次会自动变为下一个url
+        for url in start_urls:
+            yield Request(url=url,callback=self.parse)
 
     def parse(self, response):
-        print response.body
+        print response.url
+        print response
+
+
+
+
